0d78cd03fc92658b22dd4b318e85cc177d69cfdb
Thinh Dang <thinhdn.cv@gmail.com>
2024-08-28 12:05:49 +0700

    fix ant design export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    transpilePackages: [
+      "antd", 
+      "@ant-design", 
+      "rc-util", 
+      "rc-pagination", 
+      "rc-picker", 
+      "rc-notification", 
+      "rc-tooltip", 
+      "rc-tree", 
+      "rc-table"
+    ],
+    // Các cấu hình khác có thể thêm vào đây
+  };
+  
+  export default nextConfig;
+  
